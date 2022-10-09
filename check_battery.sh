8de3bc5f89fb053df1ee652ee7d01871e694e444
alfredonuhe <alfredonuhe@gmail.com>
2022-10-08 20:09:01 -0500

    fix error with status comparison

--- check_battery.sh
+++ check_battery.sh
@@ -3,14 +3,16 @@
 # This script notifies user is battery level is below a threshold and it is discharging. 
 
 # write to log when script is called
-printf 'Script started. Checking battery.' | systemd-cat -t check-battery
+echo 'Script started. Checking battery...'
 BATTERY_THRESHOLD=20
 BATTERY_CAPACITY=`cat /sys/class/power_supply/BAT1/capacity`
 BATTERY_STATUS=`cat /sys/class/power_supply/BAT1/status`
-if [[ $BATTERY_CAPACITY -le $BATTERY_THRESHOLD && $BATTERY_STATUS="Discharging" ]] ; then
+if [[ $BATTERY_CAPACITY -le $BATTERY_THRESHOLD && $BATTERY_STATUS = 'Discharging' ]] ; then
 	# write to log if condition is true
-	printf 'Battery below threshold. Running notification.' | systemd-cat -t check-battery
-	notify-send -u low "Battery Low!" "Battery is at $BATTERY_CAPACITY%. Please connect charger." --icon=dialog-warning
+	echo 'Battery below threshold. Running notification.'
+	notify-send -u low 'Battery Low!' "Battery is at $BATTERY_CAPACITY%. Please connect charger." --icon=dialog-warning
+else
+	echo 'Battery ok. Above threshold or already charging.'
 fi
 
 
